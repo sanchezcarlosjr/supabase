170f6aa47d145dd700701233d3abaf7891df90fe
sanchezcarlosjr <24639141+sanchezcarlosjr@users.noreply.github.com>
2025-01-29 20:17:59 -0800

    feat: support emailengine and directus

--- docker/volumes/db/jwt.sql
+++ docker/volumes/db/jwt.sql
@@ -1,5 +1,5 @@
 \set jwt_secret `echo "$JWT_SECRET"`
 \set jwt_exp `echo "$JWT_EXP"`
 
-select vault.create_secret(:'jwt_secret', 'app.jwt_secret', 'The jwt secret');
-select vault.create_secret(:'jwt_exp', 'app.jwt_exp', 'jwt expiration');
+--select vault.create_secret(:'jwt_secret', 'app.jwt_secret', 'The jwt secret');
+--select vault.create_secret(:'jwt_exp', 'app.jwt_exp', 'jwt expiration');
--- docker/volumes/db/roles.sql
+++ docker/volumes/db/roles.sql
@@ -6,3 +6,17 @@ ALTER USER pgbouncer WITH PASSWORD :'pgpass';
 ALTER USER supabase_auth_admin WITH PASSWORD :'pgpass';
 ALTER USER supabase_functions_admin WITH PASSWORD :'pgpass';
 ALTER USER supabase_storage_admin WITH PASSWORD :'pgpass';
+
+CREATE ROLE directus WITH PASSWORD :'pgpass';
+GRANT directus TO authenticator;
+GRANT anon TO directus;
+
+CREATE schema cms;
+GRANT USAGE ON SCHEMA cms TO directus;
+GRANT CREATE ON SCHEMA cms TO directus;
+GRANT CREATE ON SCHEMA cms TO directus;
+
+GRANT SELECT ON ALL TABLES IN SCHEMA cms TO directus;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA cms TO directus;
+ALTER DEFAULT PRIVILEGES IN SCHEMA cms
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO directus;
